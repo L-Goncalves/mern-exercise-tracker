0c9a450e63fa06984600b920cfb98f8f5f87f66d
Lucas Gonçalves <ldgoncalvs@gmail.com>
2021-02-15 18:01:49 -0300

    Adding create-exercise form

---
+++ src/components/create-user.component.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+
+export default class CreateUser extends Component{
+    constructor(){
+        super()
+
+
+    }
+
+    render() {
+        return (
+            <div>
+                a
+            </div>
+        )
+    }
+}
---
+++ src/components/edit-exercise.component.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+
+export default class EditExercise extends Component{
+    constructor(){
+        super()
+    }
+
+    render() {
+        return (
+            <div>
+                a
+            </div>
+        )
+    }
+}
